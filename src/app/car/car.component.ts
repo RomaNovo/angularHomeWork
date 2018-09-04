5a56127404f0daa865abb2e8dc4fbd46117cf545
RomaNovo <roma_1985_roma@mail.ru>
2018-09-04 15:19:18 +0300

    start

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.app-car',
+  templateUrl: './car.component.html',
+  styles: [`h2 {color: red}`]
+})
+
+export class CarComponent  {
+
+	
+}
---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent {
+	
+	
+	
+
+}
